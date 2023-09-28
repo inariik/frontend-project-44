6ccdde6060a8a6fb808fbdf06c3f22efe6b086ac
inariik <115778555+inariik@users.noreply.github.com>
2023-09-28 19:33:17 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const getRandomProgression = (length) => {
   return { progression, hiddenNumber };
 };
 
+
 const getRandomOperator = () => {
   const operators = ['+', '-', '*'];
   const randomOperator = Math.floor(Math.random() * operators.length);
